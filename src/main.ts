736169f8935fd36a715a99435073c263ccc48d7b
Aaron <a.limbo1091@gmail.com>
2026-04-29 21:37:05 -0700

    *Resolves hosted pages breaking * adds  about me section * Updates to contacts page

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,34 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { ContactComponent } from './app/pages/contact/contact.component';
 import { HomeComponent } from './app/pages/home/home.component';
 import { ProjectsComponent } from './app/pages/projects/projects.component';
-
+import { ContactComponent } from './app/pages/contact/contact.component';
+import { AboutMeComponent } from './app/pages/about-me/about-me.component';
+import Aura from '@primeuix/themes/aura';
 
 const routes = [
   { path: '', component: HomeComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'aboutMe', component: AboutMeComponent },
   { path: '**', redirectTo: '' }
 ];
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes, withHashLocation()),
+    provideHttpClient(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.app-dark'
+        }
+      }
+    })
   ]
 })
 .catch(err => console.error(err));
